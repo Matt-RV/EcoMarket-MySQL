340a7213f0688a57f03e04cfb1dc1aa017b804de
Matt-RV <mat.ramosv@duocuc.cl>
2025-06-10 23:33:28 -0400

    Arreglo de ClienteRepository

--- GestionPedidos/src/main/java/com/gestionPedidos/GestionPedidos/Repository/ClienteRepository.java
+++ GestionPedidos/src/main/java/com/gestionPedidos/GestionPedidos/Repository/ClienteRepository.java
@@ -11,6 +11,29 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+    /*  CRUD (Create, Read, Update, Delete):
+    o save(S entity): Guarda una entidad.
+    o findById(ID id): Encuentra una entidad por su ID.
+    o existsById(ID id): Verifica si una entidad con un ID dado existe.
+    o findAll(): Encuentra todas las entidades.
+    o findAllById(Iterable<ID> ids): Encuentra todas las entidades por sus IDs.
+    o count(): Cuenta todas las entidades.
+    o deleteById(ID id): Borra una entidad por su ID.
+    o delete(S entity): Borra una entidad.
+    o deleteAll(): Borra todas las entidades.
+    Paginación y Ordenación:
+            • findAll(Pageable pageable): Encuentra todas las entidades con paginación.
+            • findAll(Sort sort): Encuentra todas las entidades con ordenación.
+    */
+
+    /**
+     * Método para buscar un cliente por su ID.
+     * 
+     * @param idCliente ID del cliente a buscar
+     * @return Cliente encontrado o null si no existe
+     */
+    Cliente findByIdCliente(Integer idCliente);
+
     // Método para buscar un cliente por su nombre
     Cliente findByNombreConstraingIgnoreCase(String nombre); // Método para buscar un cliente por su nombre, ignorando mayúsculas y minúsculas
 
--- GestionPedidos/src/main/java/com/gestionPedidos/GestionPedidos/Repository/PedidoRepository.java
+++ GestionPedidos/src/main/java/com/gestionPedidos/GestionPedidos/Repository/PedidoRepository.java
@@ -10,10 +10,30 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
-    // Método para buscar pedidos por estado
-    List<Pedido> findByEstadoConstraingIgnoreCase(String estado); // Ignorando mayúsculas y minúsculas.
+    /*  CRUD (Create, Read, Update, Delete):
+    o save(S entity): Guarda una entidad.
+    o findById(ID id): Encuentra una entidad por su ID.
+    o existsById(ID id): Verifica si una entidad con un ID dado existe.
+    o findAll(): Encuentra todas las entidades.
+    o findAllById(Iterable<ID> ids): Encuentra todas las entidades por sus IDs.
+    o count(): Cuenta todas las entidades.
+    o deleteById(ID id): Borra una entidad por su ID.
+    o delete(S entity): Borra una entidad.
+    o deleteAll(): Borra todas las entidades.
+    Paginación y Ordenación:
+            • findAll(Pageable pageable): Encuentra todas las entidades con paginación.
+            • findAll(Sort sort): Encuentra todas las entidades con ordenación.
+    */
 
-    //Método para buscar clientes por ID.
+    /*
+     * Método para buscar pedidos por estado.
+     * Ignora mayúsculas y minúsculas dentro del parámetro.
+     */
+    List<Pedido> findByEstadoConstraingIgnoreCase(String estado); 
+
+    /*
+     * Método para buscar pedidos por id de cliente.
+     * Este método devuelve una lista de pedidos asociados a un cliente específico.
+     */
     List<Pedido> findByCliente_IdCliente(Integer idCliente);
-    
 }
