5b7608aa4fe7d6637c3dbb640b8cf1c283262218
Matt-RV <mat.ramosv@duocuc.cl>
2025-06-22 01:39:03 -0400

    Finalización de OAS.

--- GestionPedidos/src/main/java/com/gestionPedidos/GestionPedidos/Assemblers/PedidoModelAssembler.java
+++ GestionPedidos/src/main/java/com/gestionPedidos/GestionPedidos/Assemblers/PedidoModelAssembler.java
@@ -1,5 +1,21 @@
 package com.gestionPedidos.GestionPedidos.Assemblers;
 
-public class PedidoModelAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import com.gestionPedidos.GestionPedidos.Model.Pedido;
+import com.gestionPedidos.GestionPedidos.Controller.PedidoControllerV2;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
+@Component
+public class PedidoModelAssembler implements RepresentationModelAssembler<Pedido, EntityModel<Pedido>> {
+
+    @Override
+    public EntityModel<Pedido> toModel(Pedido pedido) { 
+        return EntityModel.of(
+            pedido,
+            linkTo(methodOn(PedidoControllerV2.class).getPedidoById(pedido.getIdPedido())).withSelfRel(),
+            linkTo(methodOn(PedidoControllerV2.class).getAllPedidos()).withRel("pedidos")
+        );
+    }
 }
--- GestionPedidos/src/main/java/com/gestionPedidos/GestionPedidos/Controller/PedidoControllerV2.java
+++ GestionPedidos/src/main/java/com/gestionPedidos/GestionPedidos/Controller/PedidoControllerV2.java
@@ -31,8 +31,39 @@ public class PedidoControllerV2 {
         List<EntityModel<Pedido>> pedidos = pedidoService.findAll().stream()
                 .map(pedidoModelAssembler::toModel)
                 .collect(Collectors.toList());
-        
         return CollectionModel.of(pedidos,
                 linkTo(methodOn(PedidoControllerV2.class).getAllPedidos()).withSelfRel());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public EntityModel<Pedido> getPedidoById(@PathVariable Integer id) { 
+        Pedido pedido = pedidoService.findById(id);
+        return pedidoModelAssembler.toModel(pedido);
+    }
+
+
+    @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<Pedido>> createPedido(@RequestBody Pedido pedido) { 
+        Pedido createdPedido = pedidoService.save(pedido);
+        return ResponseEntity
+                .created(linkTo(methodOn(PedidoControllerV2.class).getPedidoById(createdPedido.getIdPedido())).toUri())
+                .body(pedidoModelAssembler.toModel(createdPedido));
+    }
+
+
+    @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<Pedido>> updatePedido(@PathVariable Integer id, @RequestBody Pedido pedido) { 
+        pedido.setIdPedido(id);
+        Pedido updatedPedido = pedidoService.save(pedido);
+        return ResponseEntity
+            .ok(pedidoModelAssembler.toModel(updatedPedido));
+    }
+
+
+    @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<?> deletePedido(@PathVariable Integer id) { 
+        pedidoService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
