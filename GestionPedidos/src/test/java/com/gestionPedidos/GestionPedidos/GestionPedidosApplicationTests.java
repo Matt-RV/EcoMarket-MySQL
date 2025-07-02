3705f11cd879be3ecb12e2e27a01879812e4dbe3
Matt-RV <mat.ramosv@duocuc.cl>
2025-07-02 14:54:01 -0400

    final

--- GestionPedidos/src/test/java/com/gestionPedidos/GestionPedidos/GestionPedidosApplicationTests.java
+++ GestionPedidos/src/test/java/com/gestionPedidos/GestionPedidos/GestionPedidosApplicationTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+/*
+ * http://10.15.74.9:8081/api/v1/pedidos
+ * CMD -> ipconfig -> ipv4 ->
+ */
 class GestionPedidosApplicationTests {
 
 	@Test
